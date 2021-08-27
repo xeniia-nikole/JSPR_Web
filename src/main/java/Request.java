e4be31cbbefc40be93c087b625c7a4d18dd0a07a
Kseniia <xenia-nikole@mail.ru>
2021-08-27 16:17:16 +0300

    add query

--- src/main/java/Request.java
+++ src/main/java/Request.java
@@ -1,9 +1,26 @@
-import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URLEncodedUtils;
 
-public record Request(String method, String path,
-                      Map<String, String> headers) {
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+
+public final class Request {
+    private final String method;
+    private final String path;
+    private final Map<String, String> headers;
+    private final String queryString;
+    private final Map<String, List<String>> queryParams;
+
+
+    public Request(String method, String path, Map<String, String> headers,
+                   String queryString, Map<String, List<String>> queryParams) {
+        this.method = method;
+        this.path = path;
+        this.headers = headers;
+        this.queryString = queryString;
+        this.queryParams = queryParams;
+    }
 
 
     public String getMethod() {
@@ -34,8 +51,42 @@ public record Request(String method, String path,
             var headerValue = line.substring(index + 2);
             headers.put(headerName, headerValue);
         }
-        return new Request(method, path, headers);
 
+        var queryString = getQueryString(path);
+
+        var queryParams = getQueryParams(queryString);
+        System.out.println(queryParams);
+
+
+        return new Request(method, path, headers, queryString, queryParams);
+
+    }
+
+    private static String getQueryString(String path) {
+        var index = path.indexOf('?');
+        return path.substring(index + 1);
+    }
+
+    private static Map<String, List<String>> getQueryParams(String queryString) {
+        Map<String, List<String>> params = new HashMap<>();
+        var parsed = URLEncodedUtils.parse(queryString, StandardCharsets.UTF_8);
+        for (NameValuePair nameValuePair : parsed) {
+            var name = nameValuePair.getName();
+            var value = nameValuePair.getValue();
+            if (!params.containsKey(name)) {
+                params.put(name, new ArrayList<>());
+            }
+            params.get(name).add(value);
+        }
+        return params;
+    }
+
+    @Override
+    public String toString() {
+        return "Request[" +
+                "method=" + method + ", " +
+                "path=" + path + ", " +
+                "headers=" + headers + ']';
     }
 
 
